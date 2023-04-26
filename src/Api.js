2dfcb4db3a562607061a553ed872e2305220d784
Sourabh Sonawat <sourabh8820@gmail.com>
2023-04-26 11:10:03 +0530

    Initial commit

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': '867daeef64mshdfb2f3f4d664f80p1e55eajsnd793c5c67a37',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = '0da27e92dd3b34b23a86d3a4561c6c6f';
